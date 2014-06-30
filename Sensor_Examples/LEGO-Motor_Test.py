c6ee4adc5bb2500f9e511117150c4abbcc279451
Dexter Industries <dxtrindustries@gmail.com>
2014-06-30 10:48:23 +0530

    Update motor test

--- Sensor_Examples/LEGO-Motor_Test.py
+++ Sensor_Examples/LEGO-Motor_Test.py
@@ -1,6 +1,6 @@
 # Jaikrishna
 # Initial Date: June 24, 2013
-# Last Updated: June 24, 2013
+# Last Updated: June 20, 2014
 #
 # These files have been made available online through a Creative Commons Attribution-ShareAlike 3.0  license.
 # (http://creativecommons.org/licenses/by-sa/3.0/)
@@ -12,23 +12,45 @@ from BrickPi import *   #import BrickPi.py file to use BrickPi operations
 
 BrickPiSetup()  # setup the serial port for communication
 
-BrickPi.MotorEnable[PORT_A] = 1 #Enable the Motor A
-BrickPi.MotorEnable[PORT_B] = 1 #Enable the Motor B
+MotorA = PORT_A
+MotorB = PORT_B
+
+
+BrickPi.MotorEnable[MotorA] = 1 #Enable the Motor A
+BrickPi.MotorEnable[MotorB] = 1 #Enable the Motor B
 
 BrickPiSetupSensors()   #Send the properties of sensors to BrickPi
 
 while True:
     print "Running Forward"
-    BrickPi.MotorSpeed[PORT_A] = 200  #Set the speed of MotorA (-255 to 255)
-    BrickPi.MotorSpeed[PORT_B] = 200  #Set the speed of MotorB (-255 to 255)
+    BrickPi.MotorSpeed[MotorA] = 200  #Set the speed of MotorA (-255 to 255)
+    BrickPi.MotorSpeed[MotorB] = 200  #Set the speed of MotorB (-255 to 255)
     ot = time.time()
     while(time.time() - ot < 3):    #running while loop for 3 seconds
         BrickPiUpdateValues()       # Ask BrickPi to update values for sensors/motors
         time.sleep(.1)              # sleep for 100 ms
+		
+	print "Stop"
+    BrickPi.MotorSpeed[MotorA] = 0  #Set the speed of MotorA (-255 to 255)
+    BrickPi.MotorSpeed[MotorB] = 0  #Set the speed of MotorB (-255 to 255)
+    ot = time.time()
+    while(time.time() - ot < 3):    #running while loop for 3 seconds
+        BrickPiUpdateValues()       # Ask BrickPi to update values for sensors/motors
+        time.sleep(.1)              # sleep for 100 ms	
+		
     print "Running Reverse"
-    BrickPi.MotorSpeed[PORT_A] = -200  #Set the speed of MotorA (-255 to 255)
-    BrickPi.MotorSpeed[PORT_B] = -200  #Set the speed of MotorB (-255 to 255)
+    BrickPi.MotorSpeed[MotorA] = -200  #Set the speed of MotorA (-255 to 255)
+    BrickPi.MotorSpeed[MotorB] = -200  #Set the speed of MotorB (-255 to 255)
     ot = time.time()
     while(time.time() - ot < 3):    #running while loop for 3 seconds
         BrickPiUpdateValues()       # Ask BrickPi to update values for sensors/motors
         time.sleep(.1)              # sleep for 100 ms
+		
+	print "Stop"
+    BrickPi.MotorSpeed[MotorA] = 0  #Set the speed of MotorA (-255 to 255)
+    BrickPi.MotorSpeed[MotorB] = 0  #Set the speed of MotorB (-255 to 255)
+    ot = time.time()
+    while(time.time() - ot < 3):    #running while loop for 3 seconds
+        BrickPiUpdateValues()       # Ask BrickPi to update values for sensors/motors
+        time.sleep(.1)              # sleep for 100 ms	
+			
