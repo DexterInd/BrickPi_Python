879315b87fdc2c8c4721221a4a8a71d178b7a9f4
Peter Lin <woolfel@gmail.com>
2015-01-05 22:27:07 -0500

    raspberry pi doesn't support opencl
    updated cmake to turn off opencl support.

--- Project_Examples/openCV/install.sh
+++ Project_Examples/openCV/install.sh
@@ -4,12 +4,13 @@ sudo apt-get install build-essential
 sudo apt-get install python-dev python-numpy
 sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev
 sudo apt-get install libjpeg-dev libpng-dev libtiff-dev libjasper-dev
+sudo apt-get install python-scipy python-matplotlib
 sudo git clone git://github.com/Itseez/opencv.git
 
 cd opencv
 sudo mkdir release
 cd release
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D WITH_OPENCL=OFF -D CMAKE_INSTALL_PREFIX=/usr/local ..
 sudo make
 sudo make install
 
