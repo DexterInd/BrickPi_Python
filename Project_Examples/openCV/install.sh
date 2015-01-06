7a991dd78e5335ec429605d00fe57505df659bad
Peter Lin <woolfel@gmail.com>
2015-01-06 15:54:34 -0500

    install.sh is correct now
    verified the install really works and I can run the example scripts to
    detect corners

--- Project_Examples/openCV/install.sh
+++ Project_Examples/openCV/install.sh
@@ -4,7 +4,7 @@ sudo apt-get install build-essential
 sudo apt-get install python-dev python-numpy
 sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev
 sudo apt-get install libjpeg-dev libpng-dev libtiff-dev libjasper-dev
-sudo apt-get install python-scipy python-matplotlib
+sudo apt-get install python-scipy python-matplotlib libgtk2.0-dev
 sudo git clone git://github.com/Itseez/opencv.git
 
 cd opencv
