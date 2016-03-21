c45166702703db05d37eeca27ba30447bcd48b72
Dexter Industries <dxtrindustries@gmail.com>
2016-03-21 20:33:48 +0400

    Update EV3-ColorSensor.py

--- Sensor_Examples/EV3-ColorSensor.py
+++ Sensor_Examples/EV3-ColorSensor.py
@@ -6,6 +6,8 @@
 # Note you must have the latest firmware installed on the BrickPi or this example to work.  
 # The EV3 Color Sensor is attached to Port 4.
 ##
+# The Color sensor will go to sleep if it is not polled every 100ms.  This is an important point to keep in mind when designing your code.
+#
 # TYPE_SENSOR_EV3_COLOR_M0     = 50	# Reflected Light.  Shine against a surface to see the effect.
 # TYPE_SENSOR_EV3_COLOR_M1     = 51	# Ambient.  Detects ambient light, hold up to a bright light to see the effect, dark area to see the effect.
 # TYPE_SENSOR_EV3_COLOR_M2     = 52	# Color  // Min is 0, max is 7 (brown).  Returns a value for each color it sees.
@@ -44,4 +46,5 @@ while True:
 		print str(color_sensor)
 		
 	time.sleep(.01)     # sleep for 10 ms
+			    # The color sensor will go to sleep and not return proper values if it is left for longer than 100 ms.  You must be sure to poll the color sensor every 100 ms!
 
